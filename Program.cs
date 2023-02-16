9c128257cb2488497147e5be75a653373d504aac
Ong Wei Ling <ong.weiling@petronas.com>
2023-02-16 09:45:00 +0800

    Newly added csharp file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Hello, World!");
+        Console.WriteLine("This is a new sentence");
+
+       /* string userName;
+
+        Console.WriteLine("Enter username: ");
+        userName = Console.ReadLine();
+        Console.WriteLine("Username is " + userName);*/
+
+        Console.WriteLine("Enter username: ");
+        string userName = Console.ReadLine();
+        object value = Console.WriteLine("Username is " + userName);
+
+        
+    }
+}
