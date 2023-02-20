7fd003c78211e19d817480c11867dcd690411949
Ong Wei Ling <ong.weiling@petronas.com>
2023-02-20 14:46:34 +0800

    Add email

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
-﻿
-using System;
+﻿using System;
 
 namespace MyApplication
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      try{
-      // Type your username and press enter
-      Console.WriteLine("Enter username:");
+        static void Main(string[] args)
+        {
+            // Type your username and press enter
+            Console.WriteLine("Enter username:");
 
-      // Create a string variable and get user input from the keyboard and store it in the variable
-      string userName = Console.ReadLine();
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
 
-      // Print the value of the variable (userName), which will display the input value
-      Console.WriteLine("Username is: " + userName);
-      }
+            // Type your email and press enter
+            Console.WriteLine("Enter email:");
 
-      catch(Exception e) {
-        Console.WriteLine(e.Message);
-      }
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string email = Console.ReadLine();
+
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
+
+            // Print the value of the variable (email), which will display the input value
+            Console.WriteLine("Email is: " + email);
+        }
     }
-  }
 }
-
